d7b30758b11902c3a71c59aab2e6135c9b412b58
Harrison Turley <harrison.turley.1998@gmail.com>
2019-03-06 20:58:51 -0800

    Add social links to home screen

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -89,4 +89,17 @@ window.onload = function() {
         $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
         return false;
     });
+
+    $('a.js-scroll[href*="#"]:not([href="#"])').on("click", function () {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html, body').animate({
+					scrollTop: (target.offset().top - navHeight + 5)
+				}, 1000, "easeInOutExpo");
+				return false;
+			}
+		}
+	});
 })(jQuery);
